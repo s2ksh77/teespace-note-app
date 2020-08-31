eb75b8e3270b2ff71ec40435d8f397f5a5df2b95
Joo-Kwang Park <jookwang_park@tmax.co.kr>
2020-08-31 11:23:26 +0900

    feat: ES6 번들 생성을 위한 rollup 설정 추가

---
+++ src/export.js
@@ -0,0 +1,4 @@
+export { default as TalkApp } from './apps/talk/TalkApp';
+export { default as DriveApp } from './apps/drive/DriveApp';
+export { default as NoteApp } from './apps/note/NoteApp';
+export { default as CalendarApp } from './apps/calendar/CalendarApp';
