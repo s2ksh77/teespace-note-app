67d788f81dc341bbf3a23ef45962ae101b0a391f
sojeong_kim <sojeong_kim@tmax.co.kr>
2020-09-14 17:15:33 +0900

    feat:[note] 아직 커밋 안한 부분 되돌리기

--- src/apps/note/NoteApp.js
+++ src/apps/note/NoteApp.js
@@ -3,7 +3,8 @@ import LNBMenuContainer from './components/lnb/lnb';
 import useStore from './store/useStore';
 import { GlobalStyle, LNBContainer, ContentContainer } from './GlobalStyles';
 import PageEditorContainer from './components/page/pageEditorContainer';
-import TagPanelContainer from './components/tag/tagMenu';
+// import TagPanelContainer from './components/tag/tagMenu';
+import TagMenuContainer from './components/tag/tagMenu';
 import { useObserver } from 'mobx-react';
 
 const TeeNote = () => {
