0efbc5eb21797b617186ca971cd3d3c9dfce2a8f
Joo-Kwang Park <jookwang_park@tmax.co.kr>
2020-09-17 18:34:26 +0900

    chore: eslint 설정 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': 0,
     'react/prefer-stateless-function': 0,
-    'react/prop-types': 0,
+    'react/prop-types': 0,,
+    "prettier/prettier": ["error", { "endOfLine": "auto" }]
   },
 };
