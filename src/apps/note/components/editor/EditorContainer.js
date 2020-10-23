0b9953e19f4971db573ffa1f92ad96fcced7de63
sojeong_kim <sojeong_kim@tmax.co.kr>
2020-10-23 14:21:37 +0900

    feat(note): custom link 메뉴 아이콘 변경, 기존 link 메뉴 제거

--- src/apps/note/components/editor/EditorContainer.js
+++ src/apps/note/components/editor/EditorContainer.js
@@ -200,8 +200,8 @@ const EditorContainer = () => {
                   editor.editorUpload.uploadImages(handleFileBlob('image'))
                 }
               })
-              editor.ui.registry.addButton('customLinkDialog', {
-                icon:'insert-date',
+              editor.ui.registry.addButton('customLink', {
+                icon:'link',
                 tooltip:'link',
                 onAction: function () {
                   EditorStore.tinymce.execCommand('mceLink');
@@ -253,7 +253,7 @@ const EditorContainer = () => {
           onEditorChange={getEditorContent}
           apiKey="d9c90nmok7sq2sil8caz8cwbm4akovrprt6tc67ac0y7my81"
           plugins="print preview paste importcss searchreplace autolink autosave directionality code visualblocks visualchars fullscreen image link media codesample table charmap checklist hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars"
-          toolbar="undo redo | formatselect | fontselect fontsizeselect forecolor backcolor | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | numlist bullist checklist| outdent indent | link customLinkDialog | hr table insertdatetime | insertImage insertfile media"
+          toolbar="undo redo | formatselect | fontselect fontsizeselect forecolor backcolor | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | numlist bullist checklist| outdent indent | customLink | hr table insertdatetime | insertImage insertfile media"
         />
         {EditorStore.isFile ? <FileLayout /> : null}
         <TagListContainer />
