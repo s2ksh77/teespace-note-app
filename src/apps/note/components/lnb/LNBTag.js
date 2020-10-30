721e9a011aa1ab132b74743975b293f228912b64
sojeong_kim <sojeong_kim@tmax.co.kr>
2020-10-30 15:16:04 +0900

    fix(note): 페이지 수정 중 lnb 태그메뉴 클릭 시 return

--- src/apps/note/components/lnb/LNBTag.js
+++ src/apps/note/components/lnb/LNBTag.js
@@ -9,7 +9,7 @@ const filterColor =
   'invert(43%) sepia(30%) saturate(7449%) hue-rotate(174deg) brightness(93%) contrast(101%)';
 
 const LNBTag = memo(() => {
-  const { NoteStore, ChapterStore } = useStore();
+  const { NoteStore, ChapterStore, PageStore } = useStore();
 
   const [, drop] = useDrop({
     accept: 'chapter',
@@ -23,6 +23,7 @@ const LNBTag = memo(() => {
   });
 
   const onClickTagMenuBtn = () => {
+    if (PageStore.isEdit) return;
     NoteStore.setShowPage(false);
     if (NoteStore.layoutState === 'collapse') {
       NoteStore.setTargetLayout('Content');
