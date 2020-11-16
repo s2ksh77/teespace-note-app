534c1354eb6bc108061d50f750c3e72b6e6acae4
sojeong_kim <sojeong_kim@tmax.co.kr>
2020-11-16 10:51:17 +0900

    chore(note): 새 챕터 버튼 크기 고정

--- src/apps/note/styles/titleStyle.js
+++ src/apps/note/styles/titleStyle.js
@@ -102,6 +102,7 @@ export const LnbTitleCover = styled.div`
 export const LnbTitleNewButton = styled.button`
   align-items: center;
   display: flex;
+  flex-shrink:0;
   width: 6.31rem;
   height: 1.88rem;
   margin-right: 0.63rem;
