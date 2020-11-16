df0df468d4d785d89bba0aea63bf6f30caf0d92c
sojeong_kim <sojeong_kim@tmax.co.kr>
2020-11-16 11:08:37 +0900

    chore(note): 빈줄없애기

--- src/apps/note/components/tag/TagHeader.js
+++ src/apps/note/components/tag/TagHeader.js
@@ -69,8 +69,7 @@ const TagHeader = () => {
             </TagTitleSearchContainer>
           </TagSearchForm>
           <HeaderButtons />
-        </RightAligned>
-        
+        </RightAligned>        
       </TagMenuHeader>
     </>
   ));
