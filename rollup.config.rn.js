3b28976b753da3ad527c80256ed25f2330a2e847
오현석 <hyunseok_oh@tmax.co.kr>
2020-11-18 08:03:34 +0000

    Add new file

---
+++ rollup.config.rn.js
@@ -0,0 +1,43 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import image from '@rollup/plugin-image';
+import visualizer from 'rollup-plugin-visualizer';
+import url from 'rollup-plugin-url';
+import json from '@rollup/plugin-json';
+import pkg from './package.json';
+
+const extensions = ['.js', '.jsx'];
+
+process.env.BABEL_ENV = 'production';
+
+export default {
+  input: './src/external.rn.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+    },
+  ],
+  plugins: [
+    json(),
+    external(),
+    postcss(),
+    babel({
+      plugins: [
+        ['@babel/plugin-proposal-decorators', { legacy: true }],
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ],
+      exclude: 'node_modules/**',
+    }),
+    commonjs(),
+    image(),
+    visualizer(),
+    url(),
+  ],
+};
