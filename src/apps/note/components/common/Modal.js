d1185143c50a6af4a7b5ad26e7f623f92c4cb5f8
soohyun <soohyun_kim2@tmax.co.kr>
2020-12-09 17:25:50 +0900

    feat(note): 전달 팝업 ItemSelector 추가

--- src/apps/note/components/common/Modal.js
+++ src/apps/note/components/common/Modal.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {
   CustomModal,
+  RoomShareModal,
+  RoomShareTitleContainer,
+  RoomShareTitle,
   CustomOverlay,
   ModalTitle,
   ModalSubTitle,
@@ -10,6 +13,7 @@ import {
   ModalSharedInfoContent,
   ButtonGroup,
   IconImg,
+  Button,
   ModalNormalBtn,
   ModalCancelBtn
 } from '../../styles/commonStyle';
@@ -18,44 +22,92 @@ import useNoteStore from '../../store/useStore';
 import normalIcon from '../../assets/ts_info_normal@3x.png';
 import alertIcon from '../../assets/ts_popup_info_alert@3x.png';
 import defaultIcon from '../../assets/ts_info@3x.png';
+import { ItemSelector, useCoreStores } from 'teespace-core'
+import cancelImg from '../../assets/ts_cancel@3x.png';
 
 const icon = { normal: normalIcon, alert: alertIcon, default: defaultIcon };
 const Modal = () => {
   const { NoteStore } = useNoteStore();
+  const { userStore, roomStore } = useCoreStores();
   const { type, title, subTitle, buttons, sharedInfo } = NoteStore.modalInfo;
 
   let el = ReactDom.createPortal(
     <>
       <CustomOverlay />
-      <CustomModal className="NoteModal" >
-        <IconImg src={icon[type]} />
-        <ModalTitle>{title}</ModalTitle>
-        {subTitle && <ModalSubTitle>{subTitle}</ModalSubTitle>}
-        {sharedInfo && sharedInfo.map(info => {
-          return (
-            <ModalSharedInfoCover key={info.title}>
-              <ModalSharedInfoTitle>{info.title}</ModalSharedInfoTitle>
-              <ModalSharedInfoContent>{info.content}</ModalSharedInfoContent>
-            </ModalSharedInfoCover>
-          )
-        })}
-        <ButtonGroup>
-          {buttons && buttons.map(button => {
-            if (button.type === 'cancel') {
-              return (
-                <ModalCancelBtn key={button.text} onClick={button.onClick}>
-                  {button.text}
-                </ModalCancelBtn>
-              );
-            }
-            return (
-              <ModalNormalBtn key={button.text} onClick={button.onClick}>
-                {button.text}
-              </ModalNormalBtn>
-            );
-          })}
-        </ButtonGroup>
-      </CustomModal>
+      {NoteStore.isShared ? (
+        <>
+          <RoomShareModal>
+            <RoomShareTitleContainer>
+              <RoomShareTitle>다른 룸으로 전달</RoomShareTitle>
+              <Button src={cancelImg}></Button>
+            </RoomShareTitleContainer>
+            <ItemSelector
+              isVisibleRoom={false}
+              disabledIds={[
+                userStore.myProfile.id,
+                roomStore.getRoomArray()[0].id,
+                roomStore.getRoomArray()[1].id,
+              ]}
+              onSelectChange={data => {
+                console.log(data)
+              }}
+              defaultSelectedUsers={[userStore.myProfile]}
+              defaultSelectedRooms={roomStore
+                .getRoomArray()
+                .filter(roomInfo => roomInfo.isVisible)}
+            />
+            <ButtonGroup>
+              {buttons && buttons.map(button => {
+                if (button.type === 'cancel') {
+                  return (
+                    <ModalCancelBtn key={button.text} onClick={button.onClick}>
+                      {button.text}
+                    </ModalCancelBtn>
+                  );
+                }
+                return (
+                  <ModalNormalBtn key={button.text} onClick={button.onClick}>
+                    {button.text}
+                  </ModalNormalBtn>
+                );
+              })}
+            </ButtonGroup>
+          </RoomShareModal>
+        </>
+      ) : (
+          <>
+            <CustomModal className="NoteModal" >
+              <IconImg src={icon[type]} />
+              <ModalTitle>{title}</ModalTitle>
+              {subTitle && <ModalSubTitle>{subTitle}</ModalSubTitle>}
+              {sharedInfo && sharedInfo.map(info => {
+                return (
+                  <ModalSharedInfoCover key={info.title}>
+                    <ModalSharedInfoTitle>{info.title}</ModalSharedInfoTitle>
+                    <ModalSharedInfoContent>{info.content}</ModalSharedInfoContent>
+                  </ModalSharedInfoCover>
+                )
+              })}
+              <ButtonGroup>
+                {buttons && buttons.map(button => {
+                  if (button.type === 'cancel') {
+                    return (
+                      <ModalCancelBtn key={button.text} onClick={button.onClick}>
+                        {button.text}
+                      </ModalCancelBtn>
+                    );
+                  }
+                  return (
+                    <ModalNormalBtn key={button.text} onClick={button.onClick}>
+                      {button.text}
+                    </ModalNormalBtn>
+                  );
+                })}
+              </ButtonGroup>
+            </CustomModal>
+          </>
+        )
+      }
     </>,
     document.body,
   );
