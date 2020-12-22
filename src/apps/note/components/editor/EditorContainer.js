2b9cc444ea6ef9320b7833497617ba0471ba6c75
soohyun <soohyun_kim2@tmax.co.kr>
2020-12-22 11:07:39 +0900

    fix(ims): #248439 캐럿 생성 시 툴팁메뉴가 계속 호출되는 이슈

--- src/apps/note/components/editor/EditorContainer.js
+++ src/apps/note/components/editor/EditorContainer.js
@@ -375,7 +375,7 @@ const EditorContainer = () => {
             anchor_top: false, // link 입력중 dropdown으로 <top> 안뜨게 해
             anchor_bottom: false,
             extended_valid_elements: 'a[href|target=_blank]',
-            quickbars_insert_toolbar: 'insertImage table',
+            quickbars_insert_toolbar: false,
             quickbars_image_toolbar: false,
             imagetools_toolbar: 'rotateleft rotateright flipv fliph editimage changeImage | downloadImage deleteImage',
             language: 'ko_KR',
