9a1e3aaab42ab320c41c4925eea1bf4d5e286e4d
sojeong_kim <sojeong_kim@tmax.co.kr>
2021-01-20 16:19:07 +0900

    feat(note): anatd 모달과 core button으로 변경, 이미지 css 변경

--- src/apps/note/store/NoteMeta.js
+++ src/apps/note/store/NoteMeta.js
@@ -1,25 +1,79 @@
+import React from 'react';
 import NoteStore from './noteStore';
 import PageStore from './pageStore';
 import ChapterStore from './chapterStore';
 import EditorStore from './editorStore';
+import SharedModalBody from '../components/common/SharedModalBody';
+import RoomShareModal from '../components/common/RoomShareModal';
+import { Button } from 'teespace-core';
 
+/*
+  target 컴포넌트가 계속 바뀌어서 헷갈림
+  open + target 컴포넌트 이름
+*/
 const NoteMeta = {
-  openDialog(type) {
-    return this.setDialogConfig(this.setModalConfig(type), this.setEventConfig(type))
+  // antd modal prop 설정
+  openModal(type) {
+    return this.setModalConfig(type);    
   },
-  setDialogConfig(dialogType, eventList) {
+  // antd modal prop 만들기
+  setModalConfig(type) {
+    const { sharedRoomName, sharedUserName, sharedDate } = NoteStore.sharedInfo;
+    const handleCancel = function(e) {
+      e.stopPropagation();
+      NoteStore.setModalInfo(null); NoteStore.setIsShared(false);
+    }
+    switch (type) {
+      case "sharedInfo":
+        return {
+          targetComponent: "Modal",
+          title:"정보 보기",
+          children: <SharedModalBody sharedInfo={[
+            { title: '출처 룸', content: sharedRoomName },
+            { title: '전달한 멤버', content: sharedUserName },
+            { title: '전달 날짜', content: sharedDate }
+          ]} />,
+          footer:[
+            <Button key="confirm" type="solid" shape="defualt" onClick={handleCancel}>
+              확인
+            </Button>
+          ],          
+          onCancel:handleCancel,
+          className:"sharedInfoModal"
+        }
+      case "shareRoom":
+        return {
+          targetComponent: "Modal",
+          title:"다른 룸으로 전달",
+          children:<RoomShareModal handleCancel={handleCancel} />,
+          footer:null, // RoomShareModal 안으로 넣음
+          onCancel:handleCancel,
+          className:"shareRoomModal"
+        }
+      default:
+        return;
+    }
+  },
+  // core - Modal prop 설정
+  openMessage(type) {
+    return this.setMessageConfig(this.setMessageInfoConfig(type), this.setEventConfig(type))
+  },
+  // Modal(core - Message) prop 만들기
+  setMessageConfig(dialogType, eventList) {
     const buttonList = [];
-
+    // type, shape, onClick, text 
     eventList.map((event, index) => {
-      dialogType.buttonConfig[index].onClick = event;
-      buttonList.push(dialogType.buttonConfig[index])
+      dialogType.btns[index].onClick = event;
+      buttonList.push(dialogType.btns[index])
     })
+    
     return {
-      type: 'alert',
+      targetComponent: "Message",
+      type: dialogType.type,
       title: dialogType.title,
-      subTitle: dialogType.subtitle ? dialogType.subtitle : null,
-      buttons: buttonList,
-      sharedInfo: dialogType.info ? dialogType.info : null,
+      subTitle: dialogType.subtitle,
+      btns: buttonList,
+      customBadge : dialogType.customBadge
     }
   },
   setEventConfig(type) {
@@ -58,131 +112,135 @@ const NoteMeta = {
       case 'editingPage':
       case 'chapterconfirm':
       case 'titleDuplicate':
-      case 'sharedInfo':
       case 'multiFileSomeFail':
       case 'failUpload':
       case 'sizefailUpload':
+      case 'fileOpenMail':
         eventList.push(function (e) { e.stopPropagation(); NoteStore.setModalInfo(null) });
         break;
-      case 'fileDelete':
-        eventList.push(function (e) { e.stopPropagation(); EditorStore.tempDeleteFile(); NoteStore.setModalInfo(null) });
-        eventList.push(function (e) { e.stopPropagation(); NoteStore.setModalInfo(null); EditorStore.setDeleteFileConfig('', '') });
-        break;
-      case 'shareRoom':
-        eventList.push(function (e) { e.stopPropagation(); NoteStore.shareNote(); NoteStore.setIsShared(false); NoteStore.setModalInfo(null); })
-        eventList.push(function (e) { e.stopPropagation(); NoteStore.setModalInfo(null); NoteStore.setIsShared(false); });
-        break;
+      // case 'fileDelete':
+      //   eventList.push(function (e) { e.stopPropagation(); EditorStore.tempDeleteFile(); NoteStore.setModalInfo(null) });
+      //   eventList.push(function (e) { e.stopPropagation(); NoteStore.setModalInfo(null); EditorStore.setDeleteFileConfig('', '') });
+      //   break;
+      // case 'shareRoom':
+      //   eventList.push(function (e) { e.stopPropagation(); NoteStore.shareNote(); NoteStore.setIsShared(false); NoteStore.setModalInfo(null); })
+      //   eventList.push(function (e) { e.stopPropagation(); NoteStore.setModalInfo(null); NoteStore.setIsShared(false); });
+      //   break;
       default:
         break;
     }
     return eventList;
-  },
-  setButtonConfig(type) {
+  },  
+  setBtns(type) {
+    const shape="default";
+    const defaultBtn1 = { type :"solid", shape, text:'확인' }; // 버튼 한 개일 때랑 text 바꿔서 사용
+    const defaultBtn2 = { type :"default", shape, text:'취소'};
+
     switch (type) {
-      case 'delete':
-      case 'fileDelete':
-        return [{ type: 'delete', text: '삭제' }, { type: 'cancel', text: '취소' }]
+      case 'delete': // chapter랑 page
+        return [{...defaultBtn1, text:'삭제'}, defaultBtn2];
       case 'confirm':
       case 'editingPage':
       case 'chapterconfirm':
       case 'titleDuplicate':
-      case 'sharedInfoConfirm':
       case 'multiFileSomeFail':
       case 'failUpload':
       case 'sizefailUpload':
-        return [{ type: 'confirm', text: '확인' }];
+        return [defaultBtn1];
       case 'editCancel':
-        return [{ type: 'save', text: '저장' }, { type: 'notSave', text: '저장 안 함' }, { type: 'cancel', text: '취소' }]
-      case 'shareRoom':
-        return [{ type: 'share', text: '전달' }, { type: 'cancel', text: '취소' }]
+        // return [{ type: 'save', text: '저장' }, { type: 'notSave', text: '저장 안 함' }, { type: 'cancel', text: '취소' }]
+        return [{...defaultBtn1, text:'저장'}, {...defaultBtn1, text:'저장 안 함'}, defaultBtn2];
+      // case 'shareRoom':
+      //   // return [{ type: 'share', text: '전달' }, { type: 'cancel', text: '취소' }]
+      //   return [{...defaultBtn1, text:'전달'},  defaultBtn2];
+      case 'failOpenMail':
+        return [defaultBtn1];
       default:
         return;
     }
   },
-  setModalConfig(type) {
+  setMessageInfoConfig(type) {
     const userName = '';
     const fileName = EditorStore.deleteFileName;
+    // type이 error면 빨간색, error말고 다른 색이면 보라색
     const dialogType = {
-      type: 'alert',
+      type: 'default',
       title: '',
-      subtitle: '',
-      buttonConfig: []
+      subtitle: null,
+      btns: [],
+      customBadge:null
     }
     const editingUserName = PageStore.editingUserName;
-    const { sharedRoomName, sharedUserName, sharedDate } = NoteStore.sharedInfo;
     switch (type) {
       case 'chapter':
         dialogType.title = '챕터를 삭제하시겠습니까?';
         dialogType.subtitle = '챕터에 속한 페이지도 삭제됩니다.';
-        dialogType.buttonConfig = this.setButtonConfig('delete');
+        dialogType.btns = this.setBtns('delete');
         break;
       case 'page':
         dialogType.title = '페이지를 삭제하시겠습니까?';
         dialogType.subtitle = '';
-        dialogType.buttonConfig = this.setButtonConfig('delete');
+        dialogType.btns = this.setBtns('delete');
         break;
       case 'confirm':
-        dialogType.type = 'normal';
+        dialogType.type = 'info';
         dialogType.title = '삭제할 수 없습니다.';
         dialogType.subtitle = `${PageStore.editingUserName} 님이 수정 중 입니다.`;
-        dialogType.buttonConfig = this.setButtonConfig(type);
+        dialogType.btns = this.setBtns(type);
         break;
       case 'chapterconfirm':
-        dialogType.type = 'normal';
+        dialogType.type = 'info';
         dialogType.title = '삭제할 수 없습니다.';
         dialogType.subtitle = `${PageStore.editingUserCount} 명이 수정 중 입니다.`;
-        dialogType.buttonConfig = this.setButtonConfig(type);
+        dialogType.btns = this.setBtns(type);
         break;
       case 'editCancel':
         dialogType.title = '페이지를 저장하고 나가시겠습니까?';
         dialogType.subtitle = '';
-        dialogType.buttonConfig = this.setButtonConfig(type);
+        dialogType.btns = this.setBtns(type);
         break;
       case 'fileDelete':
         dialogType.title = `선택한 ${fileName} 을 삭제하시겠습니까?`;
         dialogType.subtitle = '삭제 후에는 복구할 수 없습니다.';
-        dialogType.buttonConfig = this.setButtonConfig('delete');
+        dialogType.btns = this.setBtns('delete');
         break;
       case 'titleDuplicate':
         dialogType.title = '중복된 이름이 있습니다.';
         dialogType.subtitle = '다른 이름을 입력해주세요.';
-        dialogType.buttonConfig = this.setButtonConfig('titleDuplicate');
+        dialogType.btns = this.setBtns('titleDuplicate');
         break;
       case 'editingPage':
         dialogType.title = '수정할 수 없습니다.';
         dialogType.subtitle = `${editingUserName} 님이 수정 중 입니다.`;
-        dialogType.buttonConfig = this.setButtonConfig('editingPage');
+        dialogType.btns = this.setBtns('editingPage');
         break;
-      case 'sharedInfo':
-        dialogType.info = [
-          { title: '출처 룸', content: sharedRoomName },
-          { title: '전달한 멤버', content: sharedUserName },
-          { title: '전달 날짜', content: sharedDate }
-        ]
-        dialogType.buttonConfig = this.setButtonConfig('sharedInfoConfirm');
-        break;
-      case 'shareRoom':
-        dialogType.buttonConfig = this.setButtonConfig('shareRoom');
-        break;
+      // case 'shareRoom':
+      //   dialogType.btns = this.setBtns('shareRoom');
+      //   break;
       case 'deletedPage':
         dialogType.title = '노트가 삭제되어 불러올 수 없습니다.';
         dialogType.subtitle = '';
-        dialogType.buttonConfig = this.setButtonConfig('deletedPage');
+        dialogType.btns = this.setBtns('deletedPage');
         break;
       case 'multiFileSomeFail':
         dialogType.title = '일부 파일이 업로드되지 못하였습니다.';
         dialogType.subtitle = `(${EditorStore.uploadLength}개 항목 중 ${EditorStore.failCount}개 실패)`;
-        dialogType.buttonConfig = this.setButtonConfig('multiFileSomeFail');
+        dialogType.btns = this.setBtns('multiFileSomeFail');
         break;
       case 'sizefailUpload':
         dialogType.title = '파일 첨부는 한 번에 최대 20GB까지 가능합니다.';
         dialogType.subtitle = '';
-        dialogType.buttonConfig = this.setButtonConfig('sizefailUpload');
+        dialogType.btns = this.setBtns('sizefailUpload');
         break;
       case 'failUpload':
         dialogType.title = '파일 첨부는 한 번에 30개까지 가능합니다.';
         dialogType.subtitle = '';
-        dialogType.buttonConfig = this.setButtonConfig('failUpload');
+        dialogType.btns = this.setBtns('failUpload');
+        break;
+      case 'failOpenMail':
+        dialogType.title = 'Mail로 전달할 수 없습니다.';
+        dialogType.subtitle = '외부 메일 연동 후 다시 시도해주세요.';
+        dialogType.btns = this.setBtns(type);
       default:
         break;
     }
