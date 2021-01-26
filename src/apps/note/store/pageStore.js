0294b394f8eab24674fc60ad2f701b8ef7794125
sojeong_kim <sojeong_kim@tmax.co.kr>
2021-01-26 10:34:38 +0900

    fix(ims): #251533  새 페이지 추가 후 표에 이미지를 삽입하고 저장하면 제목이 이미지 파일명으로 표시되지 않고 (표)로 표시되는 이슈, #251804

--- src/apps/note/store/pageStore.js
+++ src/apps/note/store/pageStore.js
@@ -642,7 +642,8 @@ const PageStore = observable({
       for (let i = 0; i < contentList.length; i++) {
         if (contentList[i].tagName === 'TABLE') {
           // ims 250801 : 새 페이지 추가 후 표 삽입 -> 이미지 삽입 후 저장을 누르면 제목이 이미지명으로 표시되는 이슈
-          if (!contentList[i].textContent) return '(표)';
+          const imgList = contentList[i].getElementsByTagName('img');
+          if (!contentList[i].textContent && !imgList) return '(표)';
           const tdList = contentList[i].getElementsByTagName('td');
           for (let tdIndex = 0; tdIndex < tdList.length; tdIndex++) {
             var tableTitle = this._getTableTitle(tdList[tdIndex].childNodes);
