45cf48cbad9df87e0074c17e1321936ad06f10a9
sojeong_kim <sojeong_kim@tmax.co.kr>
2021-02-01 10:54:12 +0900

    fix(note): encode, decode noteUtil 함수 변경

--- src/apps/note/NoteUtil.js
+++ src/apps/note/NoteUtil.js
@@ -4,13 +4,13 @@ const NoteUtil = {
   // 따라서 쿼리스트링 구분자로 사용되는 =,?,&은 인코딩하지 않는다
   // encodeURIComponent는 위 세 개까지 인코딩한다(쿼리스트링의 일부로 간주하여)
   encodeStr(str) {
-    return encodeURI(this.decodeStr(str));
+    return escape(encodeURIComponent(this.decodeStr(str)));
   },
   decodeStr(str) {
     let pre = str, cur;
     try {
       while (true) {
-        cur = decodeURI(pre);
+        cur = decodeURIComponent(pre);
         if (cur === pre) return cur;
         pre = cur;
       }
