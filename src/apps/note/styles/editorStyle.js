175174cbeed29868a644df9e51df537e096b9b09
sojeong_kim <sojeong_kim@tmax.co.kr>
2021-02-08 17:22:12 +0900

    chore(note): 에디터 영역이 나중에 그려져, 에디터 영역 하단의 태그와 파일레이아웃이 잠시 위에 그려졌다가 에디터 영역이 채워진 후 아래로 내려오는 현상 수정

--- src/apps/note/styles/editorStyle.js
+++ src/apps/note/styles/editorStyle.js
@@ -2,6 +2,8 @@ import { Progress } from 'antd';
 import styled, { css } from 'styled-components';
 
 export const EditorContainerWrapper = styled.div`
+  display:flex;
+  flex-direction:column;
   width:100%;
   height:100%;
   .tox-tinymce{
