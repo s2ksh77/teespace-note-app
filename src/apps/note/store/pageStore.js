90c4574d7d8af18e6cb1dfb75a0117e1d9500d4b
DaeunOh <dev.carrotoh@gmail.com>
2021-02-08 11:28:38 +0900

    chore(note): userId에 해당하는 userProfile이 없는 경우 확인하는 부분 추가

--- src/apps/note/store/pageStore.js
+++ src/apps/note/store/pageStore.js
@@ -501,7 +501,7 @@ const PageStore = observable({
 
     if (dto.USER_ID) {
       const userProfile = await UserStore.fetchProfile(dto.USER_ID);
-      this.userNick = userProfile.nick;
+      if (userProfile) this.userNick = userProfile.nick;
     }
     this.setCurrentPageId(dto.note_id);
     ChapterStore.setCurrentChapterId(dto.parent_notebook);
