84fd2634f8abe7642ea19926b63365e112f70e9f
sojeong_kim <sojeong_kim@tmax.co.kr>
2021-02-24 10:53:37 +0900

    fix(note): default 오타 수정, chapterList 그리는 부분 getChapterNum 활용하는 것으로 수정

--- src/apps/note/GlobalVariable.js
+++ src/apps/note/GlobalVariable.js
@@ -14,7 +14,7 @@ export default GlobalVariable;
 
 // 오타로 인한 버그 막기 및 타입 리스트 확인
 export const CHAPTER_TYPE = {
-  DEFUALT:'default',
+  DEFAULT:'default',
   NOTEBOOK:'notebook',
   SHARED_PAGE:'shared_page',
   SHARED:'shared'
