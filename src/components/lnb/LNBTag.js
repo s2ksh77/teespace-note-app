5d51b8e211806456df89cd2df7f3e600559d9e3c
DaeunOh <dev.carrotoh@gmail.com>
2021-04-12 18:27:22 +0900

    feat(note): lnbTag 추가

--- src/components/lnb/LNBTag.js
+++ src/components/lnb/LNBTag.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { useObserver } from 'mobx-react';
 import useNoteStore from '../../stores/useNoteStore';
 
+import { LNBTagWrapper, LNBTagIcon, LNBTagText } from '../../styles/LNBStyle';
+import tagIcon from '../../assets/add_tag.svg';
+
 const LNBTag = () => {
   const { NoteStore } = useNoteStore();
 
   return useObserver(() => (
-    <>
-      <div> LNBTag </div>
-    </>
+    <LNBTagWrapper>
+      <LNBTagIcon src={tagIcon} />
+      <LNBTagText>태그</LNBTagText>
+    </LNBTagWrapper>
   ));
 };
 
