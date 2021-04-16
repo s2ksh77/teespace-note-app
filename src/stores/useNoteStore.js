626565dc95d30789bc9f4b243035fa577c39e1ab
sojeong_kim <sojeong_kim@tmax.co.kr>
2021-04-16 18:10:11 +0900

    feat(note): notestore에 tagstore 추가

--- src/stores/useNoteStore.js
+++ src/stores/useNoteStore.js
@@ -1,9 +1,10 @@
 import NoteStore from './store/NoteStore';
 import ChapterStore from './store/ChapterStore';
 import PageStore from './store/PageStore';
+import TagStore from './store/TagStore';
 
 const useNoteStore = () => {
-  return { NoteStore, ChapterStore, PageStore };
+  return { NoteStore, ChapterStore, PageStore, TagStore };
 };
 
 export default useNoteStore;
