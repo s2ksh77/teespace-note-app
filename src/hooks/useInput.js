046377582c11f31c721857b98722c33dd472732c
sojeong_kim <sojeong_kim@tmax.co.kr>
2021-04-19 18:30:16 +0900

    feat(note): useinput 추가

---
+++ src/hooks/useInput.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import checkMaxLength from '../utils/validators';
+
+// validator :
+const useInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+  const handleChange = e => setValue(checkMaxLength(e));
+
+  return { value, handleChange };
+};
+
+export default useInput;
