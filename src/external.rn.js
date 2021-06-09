c6ce9757b523f811ca99a07854c193b7457a35a9
jaewoong_ryu <jaewoong_ryu@tmax.co.kr>
2021-06-09 11:28:47 +0900

    fix: i18n초기화를 위한 모바일용 initApp파일 추가

--- src/external.rn.js
+++ src/external.rn.js
@@ -1,3 +1,4 @@
 //export { default as NoteApp } from './apps/note/NoteApp';
 //export { default as NoteIcon } from './apps/note/components/common/NoteIcon';
 export { default as useNoteStore } from './apps/note/store/useStore';
+export { default as initI18n } from './apps/note/initApp.rn';
