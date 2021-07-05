06499486b0ed9a13bb4d29dbb5a40f4cddfb5ab1
DaeunOh <dev.carrotoh@gmail.com>
2021-07-05 16:18:49 +0900

    chore(note): 불필요한 style 제거

--- src/apps/note/styles/lnbStyle.js
+++ src/apps/note/styles/lnbStyle.js
@@ -160,38 +160,6 @@ export const TagSearchResult = styled.div`
   background-color: ${props => (props.isSelected ? '#F2EFEC' : '')};
 `;
 
-export const LnbRecycleContainer = styled.div`
-  display: flex;
-  align-items: center;
-  position: relative;
-  width: calc(100% - 1.62rem);
-  height: 2.81rem;
-  min-height: 2.81rem;
-  padding: 0rem 0.81rem 0rem 0rem;
-  font-size: 0.81rem;
-  cursor: pointer;
-  border-bottom: 0.06rem solid #eeedeb;
-  margin: 0 0.81rem;
-  order: ${props => props.order};
-  &:hover {
-    background-color: #faf8f7;
-    border-radius: 0.31rem;
-  }
-`;
-
-export const RecycleBinImg = styled.img`
-  position: absolute;
-  width: 1rem;
-  left: 1rem;
-  filter: invert(47%) sepia(14%) saturate(163%) hue-rotate(349deg)
-    brightness(96%) contrast(87%);
-`;
-
-export const RecycleBinTxt = styled.div`
-  font-weight: 500;
-  margin-left: 2.63rem;
-`;
-
 export const LNBShareBorder = styled.div`
   display: flex;
   order: ${({ order }) => order};
