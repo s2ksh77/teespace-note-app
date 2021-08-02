e5c235b23b061e0417a6546baa75042aac652a61
dongsub_youn <dongsub_youn@tmax.co.kr>
2021-08-02 10:25:51 +0900

    chore: 폴더 구조 변경에 따른 external.rn.js 파일 수정

--- src/apps/note/initApp.rn.js
+++
@@ -1,8 +0,0 @@
-import { i18nInit } from 'teespace-core';
-import i18n from './i18n/i18n';
-
-const initApp = () => {
-  i18nInit(i18n);
-};
-
-export default initApp;
--- src/external.rn.js
+++ src/external.rn.js
@@ -1,4 +1,4 @@
 //export { default as NoteApp } from './apps/note/NoteApp';
 //export { default as NoteIcon } from './apps/note/components/common/NoteIcon';
-export { default as useNoteStore } from './apps/note/store/useStore';
-export { default as initI18n } from './apps/note/initApp.rn';
+export { default as useNoteStore } from './store/useStore';
+export { default as initI18n } from './initApp.rn';
