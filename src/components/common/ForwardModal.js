368606e69cb53b0d4e395b346010be2ff847ca63
DaeunOh <dev.carrotoh@gmail.com>
2021-10-01 18:29:00 +0900

    refactor(note): 컨텍스트 메뉴 클릭하여 다른 룸으로 전달하는 로직 리팩터링

--- src/components/common/ForwardModal.js
+++ src/components/common/ForwardModal.js
@@ -1,62 +1,55 @@
 import React, { useState } from 'react';
-import {
-  RoomShareCover,
-  ButtonGroup,
-} from '../../styles/commonStyle';
 import { ItemSelector, Button } from 'teespace-core';
-import useNoteStore from '../../store/useStore';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
+import useNoteStore from '../../store/useStore';
+import { RoomShareCover, ButtonGroup } from '../../styles/commonStyle';
 
-const StyledButtonGroup = styled(ButtonGroup)`
-  margin: 1.25rem 0;
-`;
-const AddMarginBtn = styled(Button)`
-  margin-right:0.38rem;
+const ForwardButton = styled(Button)`
+  margin-right: 0.38rem;
 `;
 
 const ForwardModal = ({ handleCancel }) => {
   const { NoteStore } = useNoteStore();
   const { t } = useTranslation();
-  const [shareArraysCnt, setShareArraysCnt] = useState(false);
-  const [tooltipStr, setTooltipStr] = useState(null);
+  const [selectedItems, setSelectedItems] = useState({});
+  const [selectedCnt, setSelectedCnt] = useState(false);
 
-  const handleSelectChange = (data) => {
-    NoteStore.setShareArrays(data);
-    setShareArraysCnt(data.userArray.length + data.roomArray.length);
-    if (shareArraysCnt === 0) setTooltipStr("프렌즈/구성원/룸을 선택해 주세요");
-    else setTooltipStr(null);
-  }
-  const handleShare = (e) => {
+  const handleSelectChange = data => {
+    setSelectedItems(data);
+    setSelectedCnt(data.userArray.length + data.roomArray.length);
+  };
+
+  const handleNoteShare = e => {
     e.stopPropagation();
-    if (shareArraysCnt === 0) return;
-    NoteStore.shareNote();
+    if (selectedCnt === 0) return;
+    NoteStore.shareNote(selectedItems);
     NoteStore.setModalInfo(null);
-  }
+  };
 
   return (
     <RoomShareCover>
       <ItemSelector
-        isVisibleRoom={true}
+        isVisibleRoom
         placeholder={t('NOTE_DELIVER_TO_ANOTHER_ROOM_05')}
         onSelectChange={handleSelectChange}
       />
-      <StyledButtonGroup>
-        <AddMarginBtn
+      <ButtonGroup style={{ margin: '1.25rem 0' }}>
+        <ForwardButton
           key="share"
           type="solid"
-          shape="defualt"
-          alert={tooltipStr}
-          onClick={handleShare}
-          disabled={!shareArraysCnt}
+          onClick={handleNoteShare}
+          disabled={!selectedCnt}
         >
           {t('NOTE_DELIVER_TO_ANOTHER_ROOM_07')}
-          {shareArraysCnt > 0 && ` ${shareArraysCnt}`}
-        </AddMarginBtn>
-        <Button key="cancel" type="oulined" shape="defualt" onClick={handleCancel}>{t('NOTE_PAGE_LIST_DEL_PGE_CHPT_05')}</Button>
-      </StyledButtonGroup>
+          {selectedCnt > 0 && ` ${selectedCnt}`}
+        </ForwardButton>
+        <Button key="cancel" type="oulined" onClick={handleCancel}>
+          {t('NOTE_PAGE_LIST_DEL_PGE_CHPT_05')}
+        </Button>
+      </ButtonGroup>
     </RoomShareCover>
-  )
-}
+  );
+};
 
 export default ForwardModal;
